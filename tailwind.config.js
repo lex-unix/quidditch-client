007723bd2b7c03ae2a4d125059a412ac01f66ead
Lex Nousagi <alexmiyin@gmail.com>
2023-02-01 00:49:28 +0200

    chore(tailwind): hover state only if supported by device

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  future: {
+    hoverOnlyWhenSupported: true
+  },
   content: [
     './pages/**/*.{js,jsx,ts,tsx}',
     './components/**/*.{js,jsx,ts,tsx}'
