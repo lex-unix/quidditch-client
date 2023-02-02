6e35901e5df946107ef7abb3498b5824dc23cf69
Lex Nousagi <alexmiyin@gmail.com>
2023-02-02 22:01:53 +0200

    fix(use-posts): fix types

--- hooks/use-posts.tsx
+++ hooks/use-posts.tsx
@@ -3,16 +3,19 @@ import { API_URL } from '@/lib/constants'
 import { Post } from '@/lib/types'
 import fetcher from '@/lib/fetcher'
 
+type Data = {
+  posts: Post[]
+  totalCount: number
+}
+
 export default function UsePosts(page: number = 1, length: number = 100) {
-  const { data, isLoading, error } = useSWR<Post[]>(
+  const { data, isLoading, error } = useSWR<Data>(
     `${API_URL}/posts/get-all?page=${page}&length=${length}`,
     fetcher
   )
 
-  console.log(data)
-
   return {
-    posts: data,
+    ...data,
     isLoading,
     isError: error
   }
