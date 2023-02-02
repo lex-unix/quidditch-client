34647aaadf3ee852e5ba41bfb9f7e65dcdd7325a
Lex Nousagi <alexmiyin@gmail.com>
2023-02-02 20:17:24 +0200

    feat(use-posts): add hook for fetching posts

---
+++ hooks/use-posts.tsx
@@ -0,0 +1,17 @@
+import useSWR from 'swr'
+import { API_URL } from '@/lib/constants'
+import { Post } from '@/lib/types'
+import fetcher from '@/lib/fetcher'
+
+export default function UsePosts() {
+  const { data, isLoading, error } = useSWR<Post>(
+    API_URL + '/players/get-all',
+    fetcher
+  )
+
+  return {
+    posts: data,
+    isLoading,
+    isError: error
+  }
+}
